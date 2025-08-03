038d757506802c994d22830af715f742dcba78e0
Teresa Tavernelli <teresa.tavernelli@gmail.com>
2025-08-03 17:21:47 -0400

    Added images and CV to public folder

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,14 +8,13 @@ const config: GatsbyConfig = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.mjs
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
+    siteTitle: `Teresa Tavernelli`,
+    siteTitleAlt: `Teresa Tavernelli`,
+    siteHeadline: `Teresa Tavernelli`,
     siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
-    siteImage: `/banner.jpg`,
+    siteDescription: `Resume of Teresa Tavernelli`,
     siteLanguage: `en`,
-    author: `@lekoarts_de`,
+    author: `Teresa Tavernelli`,
   },
   trailingSlash: `always`,
   plugins: [
@@ -27,9 +26,8 @@ const config: GatsbyConfig = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Cara - @lekoarts/gatsby-theme-cara`,
-        short_name: `Cara`,
-        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        name: `Teresa Tavernelli`,
+        description: `Resume of Teresa Tavernelli`,
         start_url: `/`,
         background_color: `#141821`,
         // This will impact how browsers show your PWA/website
